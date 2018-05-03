18d0171879007abc94fb3a542e73d61f3e7873bf
Trevis Thomas <trevislynnthomas@gmail.com>
2018-05-02 21:02:14 -0600

    Second Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var hangmanWords = ["rockantansky", "furiosa", "immortanjoe"];
+
+const guessLimit = 12;
+
+var guessedLetters = [];
+var currentWordIndex;
+var guessWord = [];
+var remainingGuesses = 0;
+var gameStarted = false;
+var finished = true;
+var wins = 0;
